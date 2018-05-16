868aaae6571dc1780b6e604daa933249bdbb938f
foxleoly <yaol.leo@gmail.com>
2018-05-16 17:04:06 +0800

    update scripts

--- ex39.py
+++ ex39.py
@@ -19,3 +19,29 @@ print('-' * 10)
 
 print("NY State has: ", cities['NY'])
 print("OR State has: ", cities['OR'])
+
+print('-' * 10)
+print("Michigan's abbreviation is:", states['Michigan'])
+print("Florida's abbreviation is:", states['Florida'])
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} is abbreviated {abbrev}")
+
+print('-' * 10)
+for abbrev, city in list(cities.items()):
+    print(f"{abbrev} has the city {city}")
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+
+print('-' * 10)
+state = states.get('Texas')
+
+if not state:
+    print("Sorry, no Texas.")
+
+city = cities.get('TX', 'Does Not Exist')
+print(f"The city for the state 'TX' is: {city}")
